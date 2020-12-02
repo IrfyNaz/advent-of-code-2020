30612d191ebb273e60017781e49cff458f5464b8
Irfan Nazir <i@irfy.co.uk>
2020-12-02 09:41:10 +0000

    Python: Day2: refactor

--- python/day2.py
+++ python/day2.py
@@ -1,5 +1,8 @@
 with open("day2.in") as f:
-    lines = f.read().splitlines()
+    data = f.read().splitlines()
+
+with open('day2.in.test') as f:
+    testData = f.read().splitlines()
 
 
 def checkPart1Rule(min, max, rule, password):
@@ -9,27 +12,25 @@ def checkPart1Rule(min, max, rule, password):
     return False
 
 
-def checkPart2Rule(min, max, rule, password):
-    minCheck = password[min-1] == rule
-    maxCheck = password[max-1] == rule
-    if(minCheck + maxCheck == 1):
+def checkPart2Rule(indexA, indexB, rule, password):
+    aCheck = password[indexA-1] == rule
+    bCheck = password[indexB-1] == rule
+    if(aCheck + bCheck == 1):
         return True
     return False
 
 
-valid = 0
-for line in lines:
-    range, rule, password = line.replace(':', '').split(' ')
-    min, max = [int(x) for x in range.split('-')]
-    valid += checkPart1Rule(min, max, rule, password)
+def checkAnswer(lines, ruleFunction):
+    valid = 0
+    for line in lines:
+        range, rule, password = line.replace(':', '').split(' ')
+        a, b = [int(x) for x in range.split('-')]
+        valid += ruleFunction(a, b, rule, password)
 
-print(valid)
+    print(valid)
 
 
-valid = 0
-for line in lines:
-    range, rule, password = line.replace(':', '').split(' ')
-    min, max = [int(x) for x in range.split('-')]
-    valid += checkPart2Rule(min, max, rule, password)
-
-print(valid)
+checkAnswer(testData, checkPart1Rule)
+checkAnswer(data, checkPart1Rule)
+checkAnswer(testData, checkPart2Rule)
+checkAnswer(data, checkPart2Rule)
