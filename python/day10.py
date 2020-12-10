45569a80dc6f9c21ee1d3cc1b0cfafa5383bdc26
Irfan Nazir <i@irfy.co.uk>
2020-12-10 21:49:44 +0000

    Python: day10 refactor

--- python/day10.py
+++ python/day10.py
@@ -1,35 +1,31 @@
 
-def readFile(filename):
-    with open(filename) as f:
-        return [int(x) for x in f.read().splitlines()]
-
-
-testData = readFile('../inputs/day10.in.test')
-testData2 = readFile('../inputs/day10-2.in.test')
-data = readFile('../inputs/day10.in')
-
 
 def getDeviceJoltage(adapters):
     return max(adapters) + 3
 
 
+def readFile(filename):
+    with open(filename) as f:
+        file = [int(x) for x in f.read().splitlines()]
+        file.sort()
+        return [0] + file + [getDeviceJoltage(file)]
+
+
+testData = readFile('../inputs/day10.in.test')
+testData2 = readFile('../inputs/day10-2.in.test')
+testRichard = readFile('../inputs/day10.in.richard')
+data = readFile('../inputs/day10.in')
+
+
 def part1(adapters):
-    data = adapters.copy()
-    data.append(getDeviceJoltage(data))
-    data.sort()
-    print(data)
-    prev = 0
-    diffs = [-prev + (prev := x) for x in data]
+    prev = adapters[0]
+    diffs = [-prev + (prev := x) for x in data[1:]]
     return diffs.count(1) * diffs.count(3)
 
 
-invalidTest = part1(testData)
-print("Test part1 is: {}".format(invalidTest))
-invalidTest2 = part1(testData2)
-print("Test (2) part1 is: {}".format(invalidTest2))
-
-invalidData = part1(data)
-print("The answer to part1 is: {}".format(invalidData))
+print("Test part1 is: {}".format(part1(testData)))
+print("Test (2) part1 is: {}".format(part1(testData2)))
+print("The answer to part1 is: {}".format(part1(data)))
 
 
 def countPaths(adapters, index, count):
@@ -47,32 +43,18 @@ def countPaths(adapters, index, count):
 
 
 def part2(adapters):
-    data = adapters.copy()
-    data.append(getDeviceJoltage(data))
-    data.append(0)
-    data.sort()
-    return countPaths(data, 0, 0)
+    return countPaths(adapters, 0, 0)
 
 
 def part2dp(adapters):
-    data = adapters.copy()
-    deviceJolt = getDeviceJoltage(data)
-    data.append(deviceJolt)
-    data.append(0)
-    data.sort()
-
-    bucket = [1] + [0 for x in range(data[-1])]
-    for a in data:
+    bucket = [1] + [0 for x in range(adapters[-1])]
+    for a in adapters:
         for i in range(max(0, a-3), a):
             bucket[a] += bucket[i]
 
     return bucket[-1]
 
 
-# print("Test part2 is: {}".format(part2(testData)))
-# print("Test part2 is: {}".format(part2(testData2)))
-# print("Data part2 is: {}".format(part2(data)))
-
-print(part2dp(testData))
-print(part2dp(testData2))
-print(part2dp(data))
+print("Test 1 part2 is: {}".format(part2(testData)))
+print("Test 2 part2 is: {}".format(part2(testData2)))
+print("Data part2 is: {}".format(part2dp(data)))
