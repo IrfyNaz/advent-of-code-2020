3bec2aa50d52dd1c817db5958d29c4218152a02a
Irfan Nazir <i@irfy.co.uk>
2020-12-24 18:16:54 +0100

    Python: Day 24 - Part 2

--- python/day24.py
+++ python/day24.py
@@ -1,6 +1,12 @@
 import re
+import copy
+import collections
+
 # How to layout a Hexgrid using a normal array:
 #   https://www.redblobgames.com/grids/hexagons/
+
+WHITE = False
+BLACK = True
 
 def readFile(filename):
     with open(filename) as f:
@@ -19,30 +25,87 @@ directionMap = {
   'nw': lambda x, y, z: [x, y+1, z-1]
 }
 
+def getDim(grid):
+  return len(grid.keys()) # the maximum size of our hexagonal grid
 
-def part1(instructions):
-  dim = max([len(i) for i in instructions]) * 2 # the maximum size of our hexagonal grid
-  grid = [[[False for i in range(dim)] for j in range(dim)] for x in range(dim)]
+def getStartGrid(instructions):
+  grid = collections.defaultdict(lambda: collections.defaultdict(lambda: collections.defaultdict(lambda: False)))
+  return(grid)
+
+def countBlack(grid):
+  count = 0
+  for x in grid.values():
+    for y in x.values():
+      for z in y.values():
+          count += z
+  return count
+
+def part1(instructions, startGrid):
+  grid = copy.deepcopy(startGrid)
   for instruction in instructions:
-    x, y, z = int(dim/2), int(dim/2), int(dim/2)
+    x, y, z = 0, 0, 0
     for step in instruction:
       x, y, z = directionMap[step](x, y, z)
     # print(f"Toggling = {x} {y} {z}: {grid[x][y][z]} -> {not grid[x][y][z]}")
     grid[x][y][z] = not grid[x][y][z]
   
-  count = 0
-  for x in range(dim):
-    for y in range(dim):
-      for z in range(dim):
-        if grid[x][y][z]:
-          # print(f"{x} {y} {z}")
-          count += 1
-  return count
+  return grid
 
 testData = readFile('inputs/day24.in.test')
-testA = part1(testData)
-print(testA)
+testA = part1(testData, getStartGrid(testData))
+print(countBlack(testA))
 
-testData = readFile('inputs/day24.in')
-testA = part1(testData)
-print(testA)
+data = readFile('inputs/day24.in')
+dataA = part1(data, getStartGrid(data))
+print(countBlack(dataA))
+
+
+def getAdjacentBlackCells(cx, cy, cz, grid):
+  count = 0
+  # print(f" {cx} {cy} {cz} {getDim(grid)}")
+  for k, d in directionMap.items():
+    x, y, z = d(cx, cy, cz)
+    # print(f"    {x} {y} {z} => {grid[x][y][z]} + {count}")
+    count += grid[x][y][z]
+  return count
+
+flatten = lambda t: [item for sublist in t for item in sublist]
+
+def getPossibleCoordinates(grid):
+    xs = list(grid.keys())
+    ys = list(set(flatten([y.keys() for y in grid.values()])))
+    zs = []
+    for x, yv in grid.items():
+      for y, zv in yv.items():
+        for z, v in zv.items():
+          zs.append(z)
+    zs = list(set(zs))
+    return [xs, ys, zs] 
+
+def part2(instructions, iterations):
+  curGrid = part1(instructions, getStartGrid(instructions))
+  for i in range(iterations):
+    nextGrid = copy.deepcopy(curGrid)
+    checkGrid = copy.deepcopy(curGrid)
+
+    xs, ys, zs = getPossibleCoordinates(curGrid)
+
+    for x in range(min(xs)-2, max(xs) + 3):
+      for y in range(min(ys)-2, max(ys) + 3):
+        for z in range(min(zs)-2, max(zs)+3):
+          count = getAdjacentBlackCells(x, y, z, checkGrid)
+          if checkGrid[x][y][z] == WHITE and count == 2:
+            nextGrid[x][y][z] = BLACK
+          elif checkGrid[x][y][z] == BLACK and (count == 0 or count > 2):
+            nextGrid[x][y][z] = WHITE
+
+    curGrid = copy.deepcopy(nextGrid)
+    print(f"iteration {i+1}: {countBlack(nextGrid)}")
+  return curGrid
+
+
+ans2t = part2(testData, 100)
+print(countBlack(ans2t))
+
+ans2 = part2(data, 100)
+print(countBlack(ans2))
